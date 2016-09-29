5b5ff44c7ec34f5588a879f40fc1bcc6fb799a27
egarmin <egarminkn@gmail.com>
2016-09-29 03:26:13 +0300

    HW03 пункт 2

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -11,3 +12,27 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2015-05-30T10:00', 'Завтрак', 500, 100000);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2015-05-30T13:00', 'Обед', 1000, 100000);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2015-05-30T20:00', 'Ужин', 500, 100000);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2015-05-31T10:00', 'Завтрак', 1000, 100000);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2015-05-31T13:00', 'Обед', 500, 100000);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2015-05-31T20:00', 'Ужин', 510, 100000);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2015-06-01T14:00', 'Админ ланч', 510, 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2015-06-01T21:00', 'Админ ужин', 1500, 100001);
