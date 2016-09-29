3b733b6f3247fb2a431c91ce1f334bfdb2718385
egarmin <egarminkn@gmail.com>
2016-09-29 16:17:27 +0300

    HW03 Optional пункт 1

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,12 +3,35 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+
 /**
  * GKislin
  * 13.03.2015.
  */
 public class MealTestData {
+    public static final int MEAL_START_ID = START_SEQ + 2;
 
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+    public static final Meal USER_MEAL_1 = new Meal(MEAL_START_ID + 5, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510);
+    public static final Meal USER_MEAL_2 = new Meal(MEAL_START_ID + 4, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500);
+    public static final Meal USER_MEAL_3 = new Meal(MEAL_START_ID + 3, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000);
+    public static final Meal USER_MEAL_4 = new Meal(MEAL_START_ID + 2, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500);
+    public static final Meal USER_MEAL_5 = new Meal(MEAL_START_ID + 1, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000);
+    public static final Meal USER_MEAL_6 = new Meal(MEAL_START_ID + 0, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500);
 
+    public static final Meal ADMIN_MEAL_1 = new Meal(MEAL_START_ID + 7, LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500);
+    public static final Meal ADMIN_MEAL_2 = new Meal(MEAL_START_ID + 6, LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510);
+
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
+            (expected, actual) -> expected == actual ||
+                              (Objects.equals(expected.getCalories(), actual.getCalories())
+                            && Objects.equals(expected.getId(), actual.getId())
+                            && Objects.equals(expected.getDescription(), actual.getDescription())
+                            && Objects.equals(expected.getDateTime(), actual.getDateTime())
+                    )
+    );
 }
