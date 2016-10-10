084d06609c5f2f8034c60ab856eb4ae5e62dca1d
egarmin <egarminkn@gmail.com>
2016-10-10 18:14:53 +0300

    HW05 п.1

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,11 +1,34 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * gkislin
  * 02.10.2016
  */
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    @Modifying
+    @Query(name = Meal.DELETE)
+    int delete(@Param("id") int id,
+               @Param("userId") int userId);
+
+    @Query(name = Meal.GET)
+    List<Meal> findOne(@Param("id") int id,
+                       @Param("userId") int userId);
+
+    @Query(name = Meal.ALL_SORTED)
+    List<Meal> findAll(@Param("userId") int userId);
+
+    @Query(name = Meal.GET_BETWEEN)
+    List<Meal> findBetween(@Param("startDate") LocalDateTime startDate,
+                           @Param("endDate") LocalDateTime endDate,
+                           @Param("userId") int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -1,9 +1,16 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,33 +20,44 @@ import java.util.List;
  * 27.03.2015.
  */
 @Repository
+@Transactional(readOnly = true)
 public class DataJpaMealRepositoryImpl implements MealRepository {
 
     @Autowired
     private CrudMealRepository crudRepository;
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    @Transactional
+    public Meal save(Meal meal, int userId) {
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
+            return null;
+        }
+        meal.setUser(em.getReference(User.class, userId));
+        return crudRepository.save(meal);
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> meals = crudRepository.findOne(id, userId);
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findAll(userId);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return crudRepository.findBetween(startDate, endDate, userId);
     }
 }
